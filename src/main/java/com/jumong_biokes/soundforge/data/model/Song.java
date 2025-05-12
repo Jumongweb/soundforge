8cc9947aa64c6f01d338f16461edc66568ae7a14
Lawal Toheeb <lawaltoheeb36@gmail.com>
2025-05-12 18:35:27 +0100

    added getter and setter

--- src/main/java/com/jumong_biokes/soundforge/data/model/Song.java
+++ src/main/java/com/jumong_biokes/soundforge/data/model/Song.java
@@ -1,11 +1,15 @@
 package com.jumong_biokes.soundforge.data.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 public class Song {
     @Id
@@ -31,6 +35,7 @@ public class Song {
             joinColumns = @JoinColumn(name = "song_id"),
             inverseJoinColumns = @JoinColumn(name = "tag_id"))
     private Set<Tag> tags = new HashSet<>();
+
 
 //    @OneToMany(mappedBy = "song")
 //    private List<PlaybackHistory> playbackHistories;
